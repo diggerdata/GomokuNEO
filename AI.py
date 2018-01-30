ab7d836efc58346432f61a448d84c02e80fa7501
Oluchukwu Okafor <ojokafor@wpi.edu>
2018-01-30 18:35:57 -0500

    AI class

--- AI.py
+++ AI.py
@@ -1,1 +1,25 @@
 # All the eval functions will be here
+from Board import Board
+
+testboard = Board(15,15)
+class AI:
+    def __init__(self):
+        #initialize the AI
+        self.name=0
+        
+
+    def play(self,Board):
+        #get possible moves and play them
+        return 0
+    def Eval(self,Board):
+        #return a current score for the board
+        return 0
+
+
+
+def main():
+    testAI=AI();
+    print(testAI.play(testboard));
+
+main()
+    
