9572a6cc4aecf86dc350edd15c46d349510aa0f3
Ebenezer Ampiah <ekampiah@wpi.edu>
2018-01-30 18:30:18 -0500

    complete(for now) Board class

--- Board.py
+++ Board.py
@@ -30,21 +30,8 @@ class Board:
 
 
     def __str__(self):
-        string = ""
-        if self.win:
-            string += self.winstatement +"\n"
-        string+="  "
-        for i in range(self.size):
-            string+="{0}{1}".format(i%10, " " if i<10 else "'")
-        string +="\n"
-        i = 0
-        for x in self.board:
-            string +="{0}{1}".format(i%10," " if i<10 else "'")
-            i+=1
-            for y in x:
-                string+="{0} ".format(y)
-            string+="\n"
-        return string
+        pass
+            
 
     def __getitem__(self, coords):
         (x, y) = coords
@@ -84,66 +71,30 @@ class Board:
         return True # if no empty cells found
 
     def printBoard(self, teams):
-        print ("")
-        print ('{0} -- {1}'.format('X', teams[0]))
-        print ('{0} -- {1}'.format('O', teams[1]))
-        print ("")
-        sys.stdout.write("   ")
+        output = ''
+        output += '{0} -- {1}\n'.format('X', teams[0])
+        output += '{0} -- {1}'.format('O', teams[1])
+        output += '\n   '
         for x in range(self.width):
-            sys.stdout.write('%s ' % (chr(x+ord('A'))))
-        sys.stdout.write("\n")
+            output += '{0} '.format(chr(x+ord('A')))
+        output += "\n"
         for y in range(self.height):
-            sys.stdout.write('%02s ' % (y+1))
+            output += '{0:2d} '.format(y+1)
             for x in range(self.width):
                 if self.board[x][y].team is None:
-                    sys.stdout.write('-')
+                    output += '-'
                 else:
-                    #team_name_hash = hashlib.md5(self._field[x][y].team).hexdigest()
                     if teams.index(self._field[x][y].team) == 0:
                         team_color = 'X'
                     else:
                         team_color = 'O'
-                    sys.stdout.write(team_color)
-                sys.stdout.write(' ')
-            sys.stdout.write('\n')
-            
-        sys.stdout.flush()
+                    output += team_color
+                output += ' '
+            output += '\n'
+        print(output)
+        
 
     def getEmptyFields(self):
         return [[(x, y) for x in range(self.width)]
                         for y in range(self.height)
                         if self.isFieldOpen((x, y))]
-       
-
-class color:
-    """
-    A simple color class.
-    Initializes with a bool argument:
-    Arbitrarily, True->color is black
-                 False->color is white
-    """
-    def __init__(self, isBlack):
-        if isBlack:
-            self.isBlack = True
-            self.color = "BLACK"
-            self.symbol = "x"
-        else:
-            self.isBlack = False
-            self.color = "WHITE"
-            self.symbol = "o"
-
-    def __eq__(self, other):
-        return type(self)==type(other) and \
-           self.color==other.color
-
-    def __ne__(self,other): return not (self == other)
-
-    def __str__(self): return self.color
-
-    def __repr__(self): return str(self)
-
-    def swap(self): #swaps a color object from Black->White or reverse
-        self.__init__(not self.isBlack)
-
-    def getNot(self): #returns a color object != self
-        return color(not self.isBlack)
